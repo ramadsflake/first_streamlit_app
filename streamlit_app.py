3afe73c0e88eeeaf19268fb4a57b482377072ad3
ramadsflake <109693735+ramadsflake@users.noreply.github.com>
2022-07-21 14:37:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 straemlit.header('Breakfast Menu')
-streamlit.text('Idli, Dosa, Upma, Vada)
-streamlit.text('Idli,Masala Dosa,Semya Upma,Vada)
+streamlit.text('Idli, Dosa, Upma, Vada')
+streamlit.text('Idli,Masala Dosa,Semya Upma,Vada')
