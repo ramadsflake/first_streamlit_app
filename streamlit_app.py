9109853a6c01c8f1bf18358db57a104fe74dea25
ramadsflake <109693735+ramadsflake@users.noreply.github.com>
2022-08-16 16:49:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ import requests
 streamlit.header('Fruityvice Advice')
 fruityvice_input = streamlit.text_input('What fruit would you like information about?' , 'kiwi')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" )
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"  + fruityvice_input )
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalize)
