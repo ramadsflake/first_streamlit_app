9713a94be61dd9bda53026862c59eb46d858e9cd
ramadsflake <109693735+ramadsflake@users.noreply.github.com>
2022-08-16 16:30:21 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,5 @@ streamlit.dataframe(fruits_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+fruityvice_normalize = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalize)
